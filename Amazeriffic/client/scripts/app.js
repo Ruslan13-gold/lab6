bad401b58469e68008bccda3b7c96d058fb2dc9f
Руслан Гарифуллин <ruslan102gar@gmail.com>
2022-04-03 22:06:05 +0300

    конечный вариант part6

--- Amazeriffic/client/scripts/app.js
+++ Amazeriffic/client/scripts/app.js
@@ -86,24 +86,23 @@ var main = function (toDoObjects) {
 					$button = $("<button>").text("Добавить");
 					$button.on("click", function () {
 						var description = $input.val(),
-						tags = $tagInput.val().split(",");
-						toDoObjects.push({"description":description, "tags":tags});
-						// здесь мы отправляем быстрое сообщение на маршрут списка задач
-						$.post("todos", {}, function (response) {
-							// этот обратный вызов выполняется при ответе сервера
-							console.log("Мы отправили данные и получили ответ сервера!");
-							console.log(response);
-						
-						});
-
-						// обновление toDos
+						tags = $tagInput.val().split(","),
+						// создаем новый элемент списка задач
+						newToDo = {"description":description, "tags":tags};
+						$.post("todos", newToDo, function (result) {
+						console.log(result);
+						// нужно отправить новый объект на клиент
+						// после получения ответа сервера
+						toDoObjects.push(newToDo);
+						// обновляем toDos
 						toDos = toDoObjects.map(function (toDo) {
-							return toDo.description;
+						return toDo.description;
 						});
-
 						$input.val("");
 						$tagInput.val("");
+						});
 					});
+
 				$("main .content").append($inputLabel).append($input).append($tagLabel).append($tagInput).append($button); 
 			}
 				return false;
